f72102b9d069de6d4cb0074cf55c1aede0db40b2
anijor <anijorshrestha@gmail.com>
2016-05-23 17:54:08 +0545

    Javascript added in route page.

--- grails-app/views/route/_create.gsp
+++ grails-app/views/route/_create.gsp
@@ -15,6 +15,9 @@
                     <div class="col-sm-8">
                         <input type="text" class="form-control" id="startPoint" name="startPoint" placeholder="Example : Godawari">
                     </div>
+                    <div class="col-sm-8" id="startPointDiv">
+
+                    </div>
                 </div>
 
                 <div class="form-group">
@@ -22,12 +25,18 @@
                     <div class="col-sm-8">
                         <input type="text" class="form-control" id="finishPoint" name="finishPoint" placeholder="Example : Lele">
                     </div>
+                    <div class="col-sm-8" id="finishPointDiv">
+
+                    </div>
                 </div>
 
                 <div class="form-group">
                     <label for="difficultyLevel" class="col-sm-4 control-label" >Difficulty Level</label>
                     <div class="col-sm-8">
-                        <g:select name="difficultyLevel" id="difficultyLevel" from="${1..5}" class="form-control" noSelection="['null':'----Select Difficulty Level----']"/>
+                        <g:select name="difficultyLevel" id="difficultyLevel" from="${1..5}" class="form-control" noSelection="['':'----Select Difficulty Level----']"/>
+                    </div>
+                    <div class="col-sm-8" id="difficultyLevelDiv">
+
                     </div>
                 </div>
 
@@ -40,6 +49,9 @@
                         <div class="col-sm-6">
                             <g:select class="form-control" name="estimatedMins" id="estimatedMins" from="${1..60}" noSelection="['':'--Minutes--']"/>
                         </div>
+                    </div>
+                    <div class="col-sm-8" id="eTimeDiv">
+
                     </div>
                 </div>
 
--- web-app/js/route.js
+++ web-app/js/route.js
@@ -4,10 +4,40 @@
 
 $(document).ready(function() {
     $('#route-table').dataTable({
-    "lengthMenu": [[6,12,24,-1],[6,12,24,"ALL"]]
-});
+        "lengthMenu": [[6,12,24,-1],[6,12,24,"ALL"]]
+    });
+    $("#startPoint").on("click",function(){
+        document.getElementById('startPointDiv').innerHTML = "";
+    })
+    $("#finishPoint").click(function(){
+        document.getElementById('finishPointDiv').innerHTML = "";
+    })
+    $("#difficultyLevel").click(function(){
+        document.getElementById('difficultyLevelDiv').innerHTML = "";
+    })
+    $("#estimatedHours").click(function(){
+        document.getElementById('eTimeDiv').innerHTML = "";
+    })
+    $("#estimatedMins").click(function(){
+        document.getElementById('eTimeDiv').innerHTML = "";
+    })
 })
 
+
+//function formValidation()
+//{
+//    var sPoint = $("#startPoint").val();
+//    var fPoint = $("#finalPoint").val();
+//    var dLevel = $("#difficultyLevel").val();
+//    var eHours = $("#estimatedHours").val();
+//    var eMins = $("#estimatedMins").val();
+//
+//    if(!sPoint){
+//        $("#startPoint").focus();
+//        $("#startPoint").innerHTML("here");
+//    }
+//
+//}
 
 
 function changeRoute(){
@@ -18,7 +48,115 @@ function changeRoute(){
     document.getElementById('routeForm').reset();
 }
 
+function check(){
+    flag = true;
+    var sPoint = $("#startPoint").val();
+    var fPoint = $("#finishPoint").val();
+    var dLevel = $("#difficultyLevel").val();
+    var eHours = $("#estimatedHours").val();
+    var eMins = $("#estimatedMins").val();
+    var letters = /^[a-zA-Z]+$/;
+    console.log("dlevel="+dLevel+"\n");
+    console.log("eHours"+eHours+"\n");
+    console.log("eMins="+eMins+"\n");
+
+    if(!sPoint){
+        $("#startPoint").focus();
+        document.getElementById('startPointDiv').innerHTML = "Start point is empty!!!!";
+        flag =  false;
+    }
+    else if(!sPoint.match(letters))
+    {
+        $("#startPoint").focus();
+        document.getElementById('startPointDiv').innerHTML = "Start point is numeric!!!!";
+        flag =  false;
+    }
+    else if(!fPoint){
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is empty!!!!";
+        flag = false;
+    }
+    else if(!fPoint.match(letters))
+    {
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is numeric!!!!";
+        flag = false;
+    }
+
+   else  if(dLevel=='')
+    {
+        $("#difficultyLevel").focus();
+        document.getElementById('difficultyLevelDiv').innerHTML = "Select difficulty level";
+        flag =  false;
+    }
+   else if(eHours=='')
+    {
+        $("#estimatedHours").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        flag = false;
+    }
+    else if(eMins=='')
+    {
+        $("#estimatedMins").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        flag = false;
+    }
+    return flag;
+}
+
 function createRoute(){
+    var sPoint = $("#startPoint").val();
+    var fPoint = $("#finishPoint").val();
+    var dLevel = $("#difficultyLevel").val();
+    var eHours = $("#estimatedHours").val();
+    var eMins = $("#estimatedMins").val();
+    var letters = /^[a-zA-Z]+$/;
+    console.log("dlevel="+dLevel+"\n");
+    console.log("eHours"+eHours+"\n");
+    console.log("eMins="+eMins+"\n");
+
+    if(!sPoint){
+            $("#startPoint").focus();
+            document.getElementById('startPointDiv').innerHTML = "Start point is empty!!!!";
+            return false;
+    }
+    if(!sPoint.match(letters))
+    {
+            $("#startPoint").focus();
+            document.getElementById('startPointDiv').innerHTML = "Start point is numeric!!!!";
+            return false;
+    }
+    if(!fPoint){
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is empty!!!!";
+        return false;
+    }
+    if(!fPoint.match(letters))
+    {
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is numeric!!!!";
+        return false;
+    }
+
+    if(dLevel=='')
+    {
+        $("#difficultyLevel").focus();
+        document.getElementById('difficultyLevelDiv').innerHTML = "Select difficulty level";
+        return false;
+    }
+    if(eHours=='')
+    {
+        $("#estimatedHours").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        return false;
+    }
+    if(eMins=='')
+    {
+        $("#estimatedMins").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        return false;
+    }
+
 
     var data = $("#routeForm").serialize();
 
@@ -37,7 +175,6 @@ function createRoute(){
             alert("Error")
         }
     });
-    return false;
 }
 
 
@@ -69,7 +206,6 @@ function deleteRoute(id){
 }
 
 function editRoute(id){
-
     $.ajax({
         type:"POST",
         url:routeUrl.editRouteUrl,
@@ -86,12 +222,15 @@ function editRoute(id){
             $('#estimatedHours').val(hour);
             $('#estimatedMins').val(minute);
 
+            check();
+
             $('#saveRoute').attr("onclick","return updateRoute();");
             $('#route_id').attr("value",data.id)
 
             $('#insert-route').modal('show');
             $('#insert-route .modal-title').html("Edit this Route")
             $('#insert-route button[type=submit]').html("Save Changes")
+
         },error: function  (er) {
             alert("Error")
         }
@@ -102,9 +241,59 @@ function editRoute(id){
 }
 
 function updateRoute(){
+    var sPoint = $("#startPoint").val();
+    var fPoint = $("#finishPoint").val();
+    var dLevel = $("#difficultyLevel").val();
+    var eHours = $("#estimatedHours").val();
+    var eMins = $("#estimatedMins").val();
+    var letters = /^[a-zA-Z]+$/;
+    console.log("dlevel="+dLevel+"\n");
+    console.log("eHours"+eHours+"\n");
+    console.log("eMins="+eMins+"\n");
 
+    if(!sPoint){
+        $("#startPoint").focus();
+        document.getElementById('startPointDiv').innerHTML = "Start point is empty!!!!";
+        return false;
+    }
+    if(!sPoint.match(letters))
+    {
+        $("#startPoint").focus();
+        document.getElementById('startPointDiv').innerHTML = "Start point is numeric!!!!";
+        return false;
+    }
+    if(!fPoint){
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is empty!!!!";
+        return false;
+    }
+    if(!fPoint.match(letters))
+    {
+        $("#finishPoint").focus();
+        document.getElementById('finishPointDiv').innerHTML = "finish point is numeric!!!!";
+        return false;
+    }
+
+    if(dLevel=='')
+    {
+        $("#difficultyLevel").focus();
+        document.getElementById('difficultyLevelDiv').innerHTML = "Select difficulty level";
+        return false;
+    }
+    if(eHours=='')
+    {
+        $("#estimatedHours").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        return false;
+    }
+    if(eMins=='')
+    {
+        $("#estimatedMins").focus();
+        document.getElementById('eTimeDiv').innerHTML ="Select Hour and Minutes";
+        return false;
+    }
     var data = $("#routeForm").serialize();
-
+    check();
     $.ajax({
         type:"POST",
         url:routeUrl.updateRouteUrl,
@@ -113,8 +302,7 @@ function updateRoute(){
             if(data.messageType=="Success"){
                 alert("Updated Successfully")
                 window.location.reload(true);
-            }else{
-                alert("Error while Updating")
+            }else{alert("Error while Updating")
             }
         },error: function (er) {
             alert("Error")
